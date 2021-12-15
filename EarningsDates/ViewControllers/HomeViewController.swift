fbf76d3f87d5afb07fa537444044f47b4ba6d09e
R34P3R44 <t.adrian88@gmail.com>
2021-12-15 10:54:27 +0000

    Add new app layout

---
+++ EarningsDates/ViewControllers/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  EarningsDates
+//
+//  Created by Adrian.T on 10/12/2021.
+//
+
+import UIKit
+
+class HomeViewController: NSObject {
+
+}
---
+++ EarningsDates/ViewControllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  EarningsDates
+//
+//  Created by Adrian.T on 10/12/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
