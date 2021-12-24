0be128972f55789272a254239028a007e8dc1705
R34P3R44 <t.adrian88@gmail.com>
2021-12-24 03:25:21 +0000

    Add final design to login and signup and logged in user. Add few other styling modifications

--- EarningsDates/Helpers/Utilities.swift
+++ EarningsDates/Helpers/Utilities.swift
@@ -13,18 +13,16 @@ class Utilities {
     static func styleTextField(_ textfield:UITextField) {
         
         // Create the bottom line
-        let bottomLine = CALayer()
-        
-        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
-        
-        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
-        
+        //let bottomLine = CALayer()
+        //bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 4)
+        //bottomLine.backgroundColor = UIColor.init(red: 255/255, green: 215/255, blue: 0/255, alpha: 1).cgColor
         // Remove border on text field
-        textfield.borderStyle = .none
-        
+        //textfield.borderStyle = .none
+        textfield.layer.borderWidth = 2
+        textfield.layer.borderColor = UIColor.white.cgColor
+        textfield.layer.cornerRadius = 10
         // Add the line to the text field
-        textfield.layer.addSublayer(bottomLine)
-        
+        //textfield.layer.addSublayer(bottomLine)
     }
     
     static func styleFilledButton(_ button:UIButton) {
