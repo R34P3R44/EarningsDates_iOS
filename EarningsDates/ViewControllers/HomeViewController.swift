6dc0e453568f0a32f35be1fa40381775fa4d29e3
R34P3R44 <t.adrian88@gmail.com>
2022-01-14 00:45:12 +0000

    Add sign out method for button with error handling and transition to ViewController storyboard,

--- EarningsDates/ViewControllers/HomeViewController.swift
+++ EarningsDates/ViewControllers/HomeViewController.swift
@@ -10,13 +10,8 @@ import FirebaseAuth
 
 class HomeViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var signOutButton: UIButton!
-    
-    
-    
-    
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,18 +22,39 @@ class HomeViewController: UIViewController {
     func setUpElements() {
         
         errorLabel.alpha = 0
-        
         Utilities.styleFilledButton(signOutButton)
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
+        let auth = Auth.auth()
         
+        if error != nil {
+            
+            showError(error!)
+            
+            self.errorLabel.text = error!.localizedDescription
+            self.errorLabel.alpha = 1
+        }
+        else {
+            
+            let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? HomeViewController
+            
+            self.view.window?.rootViewController = homeViewController
+            self.view.window?.makeKeyAndVisible()
+        }
         
+        func showError(_ message:String) {
         
-        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+        }
+    
+        func transitionToHome() {
+            
+            let viewController = storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.viewController) as? ViewController
+            
+            view.window?.rootViewController = viewController
+            view.window?.makeKeyAndVisible()
+        }
     }
-    
-    
-    
-    
 }
--- EarningsDates/ViewControllers/SignUpViewController.swift
+++ EarningsDates/ViewControllers/SignUpViewController.swift
@@ -119,5 +119,3 @@ class SignupViewController: UIViewController {
     }
     
 }
-
-//this is the updated version 52336
